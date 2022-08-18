7edd4d1e8f6c5cdb45df0778202330ae40304a24
Steven Wells <Jskight@users.noreply.github.com>
2022-08-17 21:49:24 -0400

    Create Arithmetic with R.r

---
+++ DataCamp/PostgresSQL/R/Arithmetic with R.r
@@ -0,0 +1,23 @@
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# An Exponent
+2^5
+
+28 %% 6
+
+
+ # A division
+(5 + 5) / 2 
+
+# Exponentiation
+2^5
+
+# Modulo
+28 %% 6
